a13131e5a2fba3050aed7580b47c8e5a531f638f
Artem Lubyanskiy <lubyanski_artem@mail.ru>
2020-09-08 10:14:03 +0500

    0

---
+++ 0/0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WriteReadApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.Write("Введите своё имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите свой возраст: ");
+            string age = Console.ReadLine();
+            Console.WriteLine($"Ваше имя: {name}\nВаш возраст: {age}");    
+            Console.ReadKey();
+        }
+    }
+}
